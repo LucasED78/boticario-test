328c1e24902550c72146f60917a32df2b32d0ca1
Lucas Eduardo <lucased78@hotmail.com>
2020-03-12 15:47:58 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>testeasdsa</h1>
+</template>
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/, 
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    port: 9000
+  }
+}

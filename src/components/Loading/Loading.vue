f63c64cb52a253cf4ab864b6185afb644868d1d7
Lucas Eduardo <lucased78@hotmail.com>
2020-03-13 15:17:33 -0300

    [ADDED] Loading component

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="loading">
+  </div>
+</template>
+
+<style scoped>
+  .loading {
+    width: 50px;
+    height: 50px;
+    margin: 20% auto;
+    border: 1px solid rgba(46, 166, 230, 0.2);
+    border-radius: 100%;
+    border-top-color: #2EA6E6;
+    animation: spinner 1s linear infinite;
+  }
+
+  @keyframes spinner {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>

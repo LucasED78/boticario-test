c190cceceb7174769e5e49903b38b45cd24b954d
Lucas Eduardo <lucased78@hotmail.com>
2020-03-13 09:45:33 -0300

    [ADDED] Floating button component

---
+++ src/components/Button/FloatingButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="floating__button" @click="buttonClickedHandler">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      buttonClickedHandler: {
+        type: Function,
+        default: ()=>{}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .floating__button {
+    padding: 10px;
+    position: absolute;
+    right: 15px;
+    bottom: 26px;
+    border: none;
+    border-radius: 100%;
+    outline: none;
+    cursor: pointer;
+  }
+</style>

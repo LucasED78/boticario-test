d1aa3b5605b883a3233f3e05957bc9d80d10d2db
Lucas Eduardo <lucased78@hotmail.com>
2020-03-13 09:45:11 -0300

    [ADDED] Product card component

---
+++ src/components/Product/ProductCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <Card class="product__card" style="background-image: url(https://thumbs.dreamstime.com/b/beautiful-dandelion-flower-flying-feathers-colorful-bokeh-background-macro-shot-summer-nature-scene-beautiful-dandelion-147400154.jpg);">
+    <CardBody class="">
+      <CardTitle title="Produto XPTO" />
+      <div class="product__price">
+        <CardText content="R$ 20,00" />
+        <FloatingButton>
+          <font-awesome-icon icon="plus" />
+        </FloatingButton>
+      </div>
+    </CardBody>
+  </Card>
+</template>
+
+<script>
+  import Card from '../Card/Card.vue';
+  import CardTitle from '../Card/CardTitle.vue'
+  import CardBody from '../Card/CardBody.vue';
+  import CardText from '../Card/CardText.vue';
+  import FloatingButton from '../Button/FloatingButton.vue'
+
+  export default {
+    components: {
+      Card,
+      CardTitle,
+      CardBody,
+      CardText,
+      FloatingButton
+    }
+  }
+</script>
+
+<style scoped>
+  .product__card {
+    height: 390px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    background-size: cover;
+  }
+
+  .product__price {
+    margin-top: auto;
+  }
+</style>
---
+++ src/components/Product/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="product__list">
+    <ProductCard 
+      v-for="product in products"
+      :key="product.id"
+    />
+  </div>
+</template>
+
+<script>
+  import ProductCard from './ProductCard.vue';
+  
+  export default {
+    props: {
+      products: {
+        type: Array,
+        required: true
+      }
+    },
+    components: {
+      ProductCard
+    }
+  }
+</script>
+
+<style scoped>
+  .product__list {
+    display: grid;
+    grid-template-columns: repeat(3, 266px);
+    grid-gap: 20px;
+  }
+</style>

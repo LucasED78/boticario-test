a944521471b84ee20b34af8b7f8d4fd821291fb6
Lucas Eduardo <lucased78@hotmail.com>
2020-03-14 08:13:04 -0300

    [ADDED] Notification component

---
+++ src/components/Notification/Notification.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notification">
+    {{ itemCount }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      itemCount: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .notification {
+    min-width: 10px;
+    min-height: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: -5px;
+    right: -5px;
+    border-radius: 100%;
+    color: white;
+    background-color: red;
+    font-size: .7rem;
+    font-weight: bold;
+  }
+</style>

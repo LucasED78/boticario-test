ddeb92a15fbba9dfdca6647f0f0c870249ec1446
Lucas Eduardo <lucased78@hotmail.com>
2020-03-14 16:45:09 -0300

    [ADDED] New button components

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="button">
+    {{ content }}
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      content: {
+        type: String,
+        default: 'Button'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .button {
+    border: none;
+    border-radius: 30px;
+    background-color: #2EE6C1;
+    color: white;
+    font-size: 22px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    outline: none;
+  }
+</style>
---
+++ src/components/Button/IconButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="icon__button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  .icon__button {
+    width: 60px;
+    height: 60px;
+    border: none;
+    border-radius: 100%;
+    background-color: #2EE6C1;
+    color: white;
+    font-size: 22px;
+    letter-spacing: 1px;
+  }
+</style>
